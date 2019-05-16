ecea3a2de6caf8569f5c6e23c8850fa50ca142a5
tilnow <44685516+tilnow@users.noreply.github.com>
2019-05-16 23:25:43 +0300

    Update main.js

--- hello-pwa/js/main.js
+++ hello-pwa/js/main.js
@@ -9,3 +9,36 @@ window.onload = () => {
 
 
 document.getElementById("p1").innerHTML = "New text!";
+var request = indexedDB.open("library");
+
+request.onupgradeneeded = function() {
+  // The database did not previously exist, so create object stores and indexes.
+  var db = request.result;
+  var store = db.createObjectStore("books", {keyPath: "isbn"});
+  var titleIndex = store.createIndex("by_title", "title", {unique: true});
+  var authorIndex = store.createIndex("by_author", "author");
+
+  // Populate with initial data.
+  store.put({title: "Quarry Memories", author: "Fred", isbn: 123456});
+  store.put({title: "Water Buffaloes", author: "Fred", isbn: 234567});
+  store.put({title: "Bedrock Nights", author: "Barney", isbn: 345678});
+};
+
+request.onsuccess = function() {
+  db = request.result;
+};
+var tx = db.transaction("books", "readonly");
+var store = tx.objectStore("books");
+var index = store.index("by_title");
+
+var request = index.get("Bedrock Nights");
+request.onsuccess = function() {
+  var matching = request.result;
+  if (matching !== undefined) {
+    // A match was found.
+    document.getElementById("p1").innerHTML = matching.title+ matching.author;
+  } else {
+    // No match was found.
+    document.getElementById("p1").innerHTML ="oops";
+  }
+};
