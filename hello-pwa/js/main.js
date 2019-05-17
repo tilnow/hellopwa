27980c0214415e99874a488dbc24f1a11459c942
tilnow <44685516+tilnow@users.noreply.github.com>
2019-05-17 15:06:46 +0300

    reallines

--- hello-pwa/js/main.js
+++ hello-pwa/js/main.js
@@ -28,6 +28,7 @@ function addline(thing){//shoudl we be in a form? not clear as no real submit
 //maybe add one of these and yes make it a form, at least so we can change page: <input type="submit" value="Submit">
 
   page1html=page1html+thing.question;
+  thing.col.forEach(function(obj,ind){var x=0;if(obj==true)x=1;page1html=page1html+String(x)};)//use ind later when i insert real images
   page1html=page1html+"</p></div>";
 }
 xmlhttp.open("GET", './page1.json', true);
