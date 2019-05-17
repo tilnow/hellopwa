874bd412249960607af22ccd98919ab1e4a1d746
tilnow <44685516+tilnow@users.noreply.github.com>
2019-05-17 12:42:25 +0300

    Update main.js

--- hello-pwa/js/main.js
+++ hello-pwa/js/main.js
@@ -15,9 +15,17 @@ var page1data;
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         page1data = JSON.parse(this.responseText);
-        document.write(page1data[1].question);
-        document.write(page1data[1].col3);
+        page1data.forEach(function(obj){addline(obj)});
+//        document.write(page1data[1].question);
+//        document.write(page1data[1].col3);
     }
 };
+function addline(thing){
+  var x = document.createElement("P");                        // Create a <p> node
+  var t = document.createTextNode(thing.question);    // Create a text node
+  x.appendChild(t);                                           // Append the text to <p>
+  document.body.appendChild(x);                               // Append <p> to <body>
+
+}
 xmlhttp.open("GET", './page1.json', true);
 xmlhttp.send();
