1673fb1402dec3a4945e3969808a0ab48b072181
tilnow <44685516+tilnow@users.noreply.github.com>
2019-05-17 13:59:17 +0300

    create line(s)

--- hello-pwa/js/main.js
+++ hello-pwa/js/main.js
@@ -8,24 +8,27 @@ window.onload = () => {
 }
 
 
-document.getElementById("p1").innerHTML = "New text and db!+"+localStorage.getItem("data");
-localStorage.setItem("data", Date());
+//document.getElementById("p1").innerHTML = "New text and db!+"+localStorage.getItem("data");
+//localStorage.setItem("data", Date());
 var xmlhttp = new XMLHttpRequest();
 var page1data;
+var page1html="";
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         page1data = JSON.parse(this.responseText);
         page1data.forEach(function(obj){addline(obj)});
+        document.getElementById("questions").innerHTML=page1html;
 //        document.write(page1data[1].question);
 //        document.write(page1data[1].col3);
     }
 };
-function addline(thing){
-  var x = document.createElement("P");                        // Create a <p> node
-  var t = document.createTextNode(thing.question);    // Create a text node
-  x.appendChild(t);                                           // Append the text to <p>
-  document.body.appendChild(x);                               // Append <p> to <body>
+function addline(thing){//shoudl we be in a form? not clear as no real submit
+  page1html=page1html+"<div id='line"+thing.id+"'><p>";
+  page1html=page1html+'<input type="checkbox" name="cbx'+thing.id+'" value="line'+thing.id+'">''
+//maybe add one of these and yes make it a form, at least so we can change page: <input type="submit" value="Submit">
 
+  page1html=page1html+thing.question;
+  page1html=page1html+"</p></div>";
 }
 xmlhttp.open("GET", './page1.json', true);
 xmlhttp.send();
